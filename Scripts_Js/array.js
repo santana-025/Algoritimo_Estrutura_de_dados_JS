7c8c8fc33475033feca9a9a6c15c1b3f48e0365f
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-02 21:56:13 -0300

    ARRAY

--- Scripts_Js/array.js
+++ Scripts_Js/array.js
@@ -94,5 +94,60 @@ numeros.shift();
 numeros.shift();
 numeros.shift();
 numeros.shift();
-
 console.log(numeros);
+
+//Remoção Personalizada
+numeros.splice(3, 3);
+console.log(numeros);
+//Apartir do 3 remove 3items
+
+//Adição Personalizada
+numeros.splice(3, 0, 4, 5, 6, 10, 11, 12, 123);
+console.log(numeros);
+//Apartir do 3 remove 0 items e adiciona quantos quiser
+
+//Array Bidimenssional
+// Basicamente 2 Array dentro de apenas 1
+let semanas = [];
+
+let semana01 = [31, 25.5, 40, 66, 41, 88, 12];
+let semana02 = [13, 28.5, 50, 69, 14, 48, 52];
+
+semanas[0] = semana01;
+semanas[1] = semana02;
+
+// Para Acessar os ARRAYS
+console.log(semanas[1][6]);
+// semana2 item 6
+console.log(semanas[0][4]);
+// semana1 item 4
+
+//Array multidimenssional
+// Basicamente quantos Array for nescessario dentro de apenas 1
+let mes = [];
+let primeiras_semanas = []; let ultimas_semanas = [];
+
+let sem01 = [31, 25.5, 40, 66, 41, 88, 12];
+let sem02 = [13, 28.5, 50, 69, 14, 48, 52];
+let sem03 = [21, 22, 23, 24, 25, 26, 27];
+let sem04 = [223, 244, 255, 214, 288, 246, 2667];
+
+primeiras_semanas = [sem01, sem02];
+ultimas_semanas = [sem03, sem04];
+
+mes[0] = primeiras_semanas; mes[1] = ultimas_semanas;
+
+console.log(mes[0][1][4]);
+
+// o array mes ele tem posição0 as primeiras semanas, e dentro dela temos a sem01 e sem02 respectivas as 2 primeiras semanas do mes, ja na mes posição 1 tem as ultimas semanas que dentro tem a sem03 e sem04, bastas seguir o caminho da arvore para pegar qualquer valor individual.
+
+for (let x = 0; x < mes.length; x++) {
+    for (let y = 0; y < mes[x].length; y++) {
+        for (let z = 0; z < mes[x][y].length; z++) {
+            console.log(mes[x][y][z]);
+        }
+    }
+}
+
+// Percorrendo todo o array !!
+
